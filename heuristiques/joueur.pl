a58d5eacab08573cbd35fddde5e69490e40d8ba9
fitz35 <clement@lahoche.info>
2021-11-23 11:21:52 +0100

    fix joueur input

--- heuristiques/joueur.pl
+++ heuristiques/joueur.pl
@@ -1,3 +1,16 @@
+% DE hexanome 4124
+
+% valide un input
+valid(Cout, G) :- Cout > 0, Cout < 8, nth1(Cout, G, Colonne), compter(Colonne, NbZero), NbZero > 0.
+
+% demande un input et le valide
+ask(Cout, G) :- 
+    repeat, 
+        ecrit(" entrez un nombre: ",1),
+        read(Cout),
+    valid(Cout, G),
+    !.
+
 % DE :
 % Damien Carreau
 % Enzo Boscher
@@ -6,7 +19,7 @@
 % Mickael Ben Said
 % Antoine Mandin
 
-jouerJoueur(G,Joueur, N1, N2, Etat,Res) :- ecrit("Joueur joue ",1),ecrit(Joueur,1), ecrit(" entrez un nombre: ",1),
-                                read(L), nth1(L,G,C), ajouter(C, Joueur, C1), changeColonne(G,L,C1,[],1,G1),
+jouerJoueur(G,Joueur, N1, N2, Etat,Res) :- ecrit("Joueur joue ",1),ecrit(Joueur,1),
+                                ask(L, G) , nth1(L,G,C), ajouter(C, Joueur, C1), changeColonne(G,L,C1,[],1,G1),
                                 affiche(G1,[],Etat),
                                 joueurOppose(Joueur, JoueurOp), heuristique(G1, JoueurOp, [N1|N2],Etat,Res). % gagnant()
